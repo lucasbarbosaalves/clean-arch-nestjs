fe565d27ddbfd930394314d7a086528a9682a4f7
lucasbarbosaalves <lkab05@hotmail.com>
2023-09-13 14:11:09 -0300

    config dev environment

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,12 +15,16 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', 'dis', 'node_ modules'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-namespace': 'off',
   },
   'prettier/prettier': [
     'error',
